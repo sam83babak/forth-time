819ee89a86a94a1d03715b225e0b5179ab6c72d0
sam83babak <sam83babak@gmail.com>
2024-04-12 22:55:07 +0330

    fixed layout and made a new cshtml for Contact

--- DevCard_Project/Dev_Card_MvC/Controllers/HomeController.cs
+++ DevCard_Project/Dev_Card_MvC/Controllers/HomeController.cs
@@ -14,10 +14,15 @@ namespace Dev_Card_MvC.Controllers
             return View();
         }
 
-     
-        
+		public IActionResult Contact()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
+
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
