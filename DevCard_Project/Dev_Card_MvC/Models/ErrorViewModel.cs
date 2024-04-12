6ae17c263d7313f17a7b0426e99135a90d9b8db9
sam83babak <sam83babak@gmail.com>
2024-04-12 22:30:11 +0330

    delete all of the loggers

---
+++ DevCard_Project/Dev_Card_MvC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dev_Card_MvC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
