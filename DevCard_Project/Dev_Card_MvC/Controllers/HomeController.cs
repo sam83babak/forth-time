ab730b81a099f11e9dbb62986b66cde7c2a41059
sam83babak <sam83babak@gmail.com>
2024-04-12 22:42:30 +0330

    delete privacy cshtml and its controller

--- DevCard_Project/Dev_Card_MvC/Controllers/HomeController.cs
+++ DevCard_Project/Dev_Card_MvC/Controllers/HomeController.cs
@@ -18,10 +18,8 @@ namespace Dev_Card_MvC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+     
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
