67dc64d0aba54eaf3731d6ea2a65ea74ca3768a2
sam83babak <sam83babak@gmail.com>
2024-04-13 09:43:45 +0330

    made new cs and webpage to show projects

---
+++ DevCard_Project/Dev_Card_MvC/Views/Home/Projects.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dev_Card_MvC.Views.Home
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
