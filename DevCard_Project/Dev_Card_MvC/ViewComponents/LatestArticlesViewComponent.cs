68d56f6454d7273116cb1d72ccd455fc67bfba62
sam83babak <sam83babak@gmail.com>
2024-04-15 23:25:43 +0330

    fixed the missing model in LatestArticlesViewComponents which didn't sent to cshtml and fix some Image strings (add a .jpg) and also fix html img src (add ~ inorder to work on root)

--- DevCard_Project/Dev_Card_MvC/ViewComponents/LatestArticlesViewComponent.cs
+++ DevCard_Project/Dev_Card_MvC/ViewComponents/LatestArticlesViewComponent.cs
@@ -11,14 +11,17 @@ namespace Dev_Card_MvC.ViewComponents
 
 			var articles = new List<Articale>() {
 
-			new Articale ( 1, "Salam","Salam man khobam shoma khobi","blog-post-thumb-1"),
-			new Articale ( 2, "Shoma ","سلام من به تو یار قدیمی","blog-post-thumb-2"),
-			new Articale ( 3, "‌better Call saul "," بزنگید به سال ","blog-post-thumb-3"),
-			new Articale ( 4, "Chease","yo whats up","blog-post-thumb-4"),
+			new Articale ( 1, "Salam","Salam man khobam shoma khobi","blog-post-thumb-1.jpg"),
+			new Articale ( 2, "Shoma ","سلام من به تو یار قدیمی","blog-post-thumb-2.jpg"),
+			new Articale ( 3, "‌better Call saul "," بزنگید به سال ","blog-post-thumb-3.jpg"),
+			new Articale ( 4, "Chease","yo whats up","blog-post-thumb-4.jpg"),
 
 
 			};
 
-			return View("_LatestArticles"); }
+			return View("_LatestArticles",articles);
+				
+				
+				}
 	}
 }
