6baba2a93bddeb49087517d047d5cfc137967caa
sam83babak <sam83babak@gmail.com>
2024-04-16 21:23:31 +0330

    Learned how to add new Componenets and how does some Resaults type works

---
+++ DevCard_Project/Dev_Card_MvC/Controllers/TestController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System.Net.Mime;
+
+
+namespace Dev_Card_MvC.Controllers
+{
+	public class TestController : Controller
+	{
+		//public IActionResult Index()
+		//{
+		//	return PartialView("Footer");
+		//}
+
+		//public PartialViewResult Index()
+		//{
+		//	return PartialView("Footer");
+
+		//}
+
+		//public ContentResult Index()
+		//{
+		//	return Content("<h1> Hello my name is what my name is who </h1>");
+		//	return Content("<h1 > Hello my name is what my name is who </h1>", contentType: "text/html");
+
+		//}
+
+		//public EmptyResult Index()
+		//{
+		//	return new EmptyResult();
+		//}
+
+		//public FileResult Index(){
+
+		//	return File(virtualPath:@"wwwroot/assets/images/promo-banner.jpg",contentType:"image/html");
+		//}
+
+		//public FileResult Index()
+		//{
+		//	var filebyte = System.IO.File.ReadAllBytes("wwwroot/salam.txt");
+		//	const string filename = "salam.txt";
+		//	return File(filebyte, MediaTypeNames.Text.Plain, filename);
+		//}
+
+		public FileResult Index()
+		{
+			var filebyte = System.IO.File.ReadAllBytes("wwwroot/assets/images/promo-banner.jpg");
+			const string filename = "salam.jpg";
+			return File(filebyte, MediaTypeNames.Image.Jpeg, filename);
+		}
+
+	}
+}
